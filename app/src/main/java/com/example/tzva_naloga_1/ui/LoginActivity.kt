113f6847a974ceba79892b48414be37f57d7a399
Ana <ana.plecko@student.um.si>
2022-05-23 19:22:08 +0200

    added auth with google account

--- app/src/main/java/com/example/tzva_naloga_1/ui/LoginActivity.kt
+++ app/src/main/java/com/example/tzva_naloga_1/ui/LoginActivity.kt
@@ -1,40 +1,94 @@
 package com.example.tzva_naloga_1.ui
 
-import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.example.tzva_naloga_1.R
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
+import com.example.tzva_naloga_1.databinding.ActivityLoginBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
-import com.example.tzva_naloga_1.databinding.ActivityLoginBinding
-
+import com.google.firebase.auth.GoogleAuthProvider
+import java.lang.Exception
+@Suppress("DEPRECATION")
 class LoginActivity : AppCompatActivity() {
     private lateinit var user: FirebaseAuth
     private lateinit var binding: com.example.tzva_naloga_1.databinding.ActivityLoginBinding
+    private lateinit var googleSignInClient: GoogleSignInClient;
+
+    private companion object{
+        private const val RC_SIGN_IN=100
+        private const val TAG="GOOGLE_SIGN_IN_TAG"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val googleSignInOptions=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient= GoogleSignIn.getClient(this, googleSignInOptions)
+
         user = FirebaseAuth.getInstance();
 
         binding.registerBtn.setOnClickListener{
             registerUser()
         }
+        binding.signInButton.setOnClickListener{
+            val intent=googleSignInClient.signInIntent
+            startActivityForResult(intent, RC_SIGN_IN)
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode== RC_SIGN_IN){
+            Log.d(TAG, "onActivity result ")
+            val accountTask=GoogleSignIn.getSignedInAccountFromIntent(data)
+            try{
+                val account= accountTask.getResult(ApiException::class.java)
+                firebaseAuthWithGoogleAccount(account)
+            }catch (e:Exception){
+                Log.d(TAG, "Error: " + e.message)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogleAccount(account: GoogleSignInAccount?){
+        val credential =  GoogleAuthProvider.getCredential(account!!.idToken, null);
+        user.signInWithCredential(credential)
+            .addOnSuccessListener { authResult->
+                Log.d(TAG, "Logged in ")
+
+                val firebaseUser=user.currentUser
+                val uid=firebaseUser!!.uid
+                val email=firebaseUser!!.uid
+
+                if(authResult.additionalUserInfo!!.isNewUser){
+                    Log.d(TAG, "User is new account created - email: " +email)
+
+                }else{
+                    Log.d(TAG, "Existing user - email: " +email)
+                }
+                startActivity((Intent(this, MainActivity::class.java)))
+            }
+            .addOnFailureListener{e->
+                Log.d(TAG, "Login failed " +e.message)
+            }
+    }
+
+
+
+
     private fun registerUser(){
         val email= binding.editTextTextEmailAddress.text.toString().trim()
         val password=binding.editTextTextPassword.text.toString().trim()
@@ -52,7 +106,6 @@ class LoginActivity : AppCompatActivity() {
                             if (mTask.isSuccessful){
                                 startActivity(Intent(this, MainActivity::class.java))
                                 Toast.makeText(this, "Welcome user " + user.currentUser?.email, Toast.LENGTH_SHORT).show()
-
                             }else{
                                 Toast.makeText(this, mTask.exception!!.message, Toast.LENGTH_SHORT).show()
                             }
